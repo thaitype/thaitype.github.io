2f6505f13bdd1f60683ed100aa4e2c17e3c0e962
Thada Wangthammang <thada.wth@gmail.com>
2024-05-07 21:33:05 +0700

    chore(home): upgrade from next 13 to 14

--- apps/home/app/page.tsx
+++ apps/home/app/page.tsx
@@ -1,8 +1,6 @@
 import styles from './page.module.css';
-import { api } from './api-util';
 
 export default async function Index() {
-  // const data = await api.greeting.getGreeting.query();
   return (
     <div className={styles['page']}>
       <h1>Hello</h1>
